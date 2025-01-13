057ec60c518824d0064f6b7621abc6ea3213ce79
adandoang <akhdanjzif@gmail.com>
2025-01-13 09:08:26 +0700

    membuat data class formerrorstate

--- app/src/main/java/com/example/meet14firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/meet14firebase/ui/viewmodel/InsertViewModel.kt
@@ -6,6 +6,21 @@ import com.example.meet14firebase.repository.MahasiswaRepository
 class InsertViewModel (
     private val mhs: MahasiswaRepository
 ) : ViewModel() {
+
+    data class FormErrorState (
+        val nim: String? = null,
+        val nama: String? = null,
+        val jenisKelamin: String? = null,
+        val alamat: String? = null,
+        val kelas: String? = null,
+        val angkatan: String? = null
+    ) {
+        fun isValid(): Boolean {
+            return nim == null && nama == null && jenisKelamin == null && alamat == null &&
+                    kelas == null && angkatan == null
+        }
+    }
+
     data class MahasiswaEvent(
         val nim: String = "",
         val nama: String = "",
