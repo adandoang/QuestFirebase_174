a30ab7bf90b9da450787f6d4a3644ffc23ecf943
adandoang <akhdanjzif@gmail.com>
2025-01-13 09:23:00 +0700

    membuat fungsi validatefields dan updatestate

--- app/src/main/java/com/example/meet14firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/meet14firebase/ui/viewmodel/InsertViewModel.kt
@@ -1,11 +1,41 @@
 package com.example.meet14firebase.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.meet14firebase.model.Mahasiswa
 import com.example.meet14firebase.repository.MahasiswaRepository
+import java.text.Normalizer.Form
 
 class InsertViewModel (
     private val mhs: MahasiswaRepository
 ) : ViewModel() {
+
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent
+        )
+    }
+
+    fun validateFields(): Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return  errorState.isValid()
+    }
 
     sealed class FormState {
         object Idle : FormState()
@@ -40,5 +70,14 @@ class InsertViewModel (
         val alamat: String = "",
         val kelas: String = "",
         val angkatan: String = ""
+    )
+
+    fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa (
+//        nim = nim,
+//        nama = nama,
+//        jenisKelamin = jenisKelamin,
+//        alamat = alamat,
+//        kelas = kelas,
+//        angkatan = angkatan
     )
 }
