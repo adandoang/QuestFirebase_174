98350074a1b90302f0d2ff7d96a47201e68365f2
adandoang <akhdanjzif@gmail.com>
2025-01-13 09:12:12 +0700

    membuat sealed class FormState

--- app/src/main/java/com/example/meet14firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/meet14firebase/ui/viewmodel/InsertViewModel.kt
@@ -7,6 +7,13 @@ class InsertViewModel (
     private val mhs: MahasiswaRepository
 ) : ViewModel() {
 
+    sealed class FormState {
+        object Idle : FormState()
+        object Loading : FormState()
+        data class Success(val message: String) : FormState()
+        data class Error(val message: String) : FormState()
+    }
+
     data class InsertUiState (
         val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
         val isEntryValid: FormErrorState = FormErrorState(),
