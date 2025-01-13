ff1898b37fffd7b97839cd059740838d609b6183
adandoang <akhdanjzif@gmail.com>
2025-01-13 09:49:51 +0700

    mendaftarkan insertviewmodel di PenyediaViewModel

--- app/src/main/java/com/example/meet14firebase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/meet14firebase/ui/viewmodel/PenyediaViewModel.kt
@@ -7,10 +7,20 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.meet14firebase.MahasiswaApplications
+import com.example.meet14firebase.model.Mahasiswa
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer {
+            HomeViewModel(
+                aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
+        initializer {
+            InsertViewModel(
+                aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
     }
     fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
